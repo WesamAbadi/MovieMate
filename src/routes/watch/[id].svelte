9378fd877ba79822e7903e5fe41fbd421a0a7903
Wesam Abadi <wisamabady7@gmail.com>
2023-12-13 12:44:53 +0100

    add bg image

--- src/routes/watch/[id].svelte
+++ src/routes/watch/[id].svelte
@@ -10,6 +10,7 @@
   let movieData;
   let watchId = id;
   let type = "tv";
+  let bgURL = ``;
 
   const url = `https://omdbapi.com/?i=${watchId}&apikey=${apiKey}`;
 
@@ -19,14 +20,16 @@
       ? urlParts[urlParts.indexOf("watch") + 1]
       : null;
 
-    if (urlType === "movie") {
-      type = urlType;
-    }
     try {
       const response = await fetch(url);
       const data = await response.json();
       if ("Title" in data) {
         movieData = data;
+        bgURL = movieData.Poster;
+        document.body.style.backgroundImage = `url('${bgURL}')`;
+        applyBodyAfterStyles();
+        if (movieData.Type === "movie") type = "movie";
+        else type = "tv";
       } else {
         console.error("Invalid response format");
       }
@@ -34,9 +37,24 @@
       console.error("Error fetching movie data:", error);
     }
     console.log(movieData);
-    if (type === "movie") {
-      console.log("movie");
-    }
+  };
+
+  const applyBodyAfterStyles = () => {
+    const styleElement = document.createElement("style");
+    styleElement.textContent = `
+      body::after {
+        -webkit-backdrop-filter: blur(20px);
+        backdrop-filter: blur(20px);
+        filter: brightness(0.4);
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+      }
+    `;
+    document.head.appendChild(styleElement);
   };
 
   onMount(() => {
@@ -44,28 +62,56 @@
   });
 </script>
 
+<div class="header">
+  <a href="/">MovieMate</a>
+</div>
 <main>
   {#if movieData}
-    <a href="/"><h2>MovieMate</h2></a>
-    <h1>{movieData.Title}</h1>
-    <p>{movieData.Plot}</p>
-    <iframe
-      src="https://vidsrc.to/embed/{type}/{watchId}"
-      title="Iframe example"
-      frameborder="0"
-      allowfullscreen
-      width="100%"
-      height="700px"
-      class="movie"
-      id="testframe"
-    ></iframe>
+    <div class="movie-container">
+      <div class="right">
+        <h1>{movieData.Title}</h1>
+        <p>{movieData.Plot}</p>
+      </div>
+      <div class="left">
+        <iframe
+          src="https://vidsrc.to/embed/{type}/{watchId}"
+          title="Iframe example"
+          frameborder="0"
+          allowfullscreen
+          width="100%"
+          height="700px"
+          class="movie"
+          id="testframe"
+        ></iframe>
+      </div>
+    </div>
   {:else}
     <p>Loading...</p>
   {/if}
 </main>
 
 <style>
+  .header {
+    margin: 1rem 0;
+  }
+  .movie-container {
+    display: flex;
+    flex-direction: row-reverse;
+  }
   iframe {
     border-radius: 2rem;
+    box-shadow: 1px 4px 30px rgb(0 0 0 / 62%);
+  }
+  h1 {
+    margin: 1rem 0;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 1;
+    display: grid;
+    align-content: center;
+    padding: 15px;
   }
 </style>
